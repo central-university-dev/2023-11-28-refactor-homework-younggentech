b6a86db8b7d7cb7888c134e4a4358ee72e7d20df
Alexandr Ovsyannikov <alexov2004@gmail.com>
2023-12-08 21:51:23 +0400

    hw done (updated test paths)

--- tests/test_entry.py
+++ tests/test_entry.py
@@ -1,26 +1,27 @@
 import pathlib
 from pathlib import Path
 from renamer.entry import update_classname
+FIXTURE_PATH = "./tests/fixtures/"
 
 
 def test_rename():
     got = update_classname(
-        './fixtures/source.py',
-        'A',
-        'B',
+        FIXTURE_PATH + "source.py",
+        "A",
+        "B",
     )
 
-    assert got == Path('./fixtures/expected.py').read_text()
-    assert pathlib.Path('./fixtures/source_refactorred_.py').exists()
+    assert got == Path(FIXTURE_PATH + "expected.py").read_text()
+    assert pathlib.Path(FIXTURE_PATH + "source_refactorred_.py").exists()
 
 
 def test_rename_with_imports():
     update_classname(
-        './fixtures/source.py',
-        'A',
-        'B',
-        './fixtures/'
+        FIXTURE_PATH + "source.py",
+        "A",
+        "B",
+        FIXTURE_PATH
     )
-    f_got = pathlib.Path('./fixtures/import_source_refactorred_.py')
+    f_got = pathlib.Path(FIXTURE_PATH + "import_source_refactorred_.py")
     assert f_got.exists()
-    assert f_got.read_text() == pathlib.Path('./fixtures/import_expected.py').read_text()
+    assert f_got.read_text() == pathlib.Path(FIXTURE_PATH + "import_expected.py").read_text()
